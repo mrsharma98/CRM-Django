c16f826f04f7dda14f9555a3db914f0d7f570b2e
Manish Sharma <mrsharma98@gmail.com>
2020-12-01 01:00:11 +0530

    created reg and login

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,8 @@
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
 from .models import Order
 
 class OrderForm(ModelForm):
@@ -7,3 +11,10 @@ class OrderForm(ModelForm):
         fields = '__all__'
         # if we want specific fields
         # fileds = ['customer', 'product']
+
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+        
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,36 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib import messages
 
 from .models import *
 from .forms import *
+from .filters import OrderFilter
+
 # Create your views here.
 
+def registerPage(request):
+    # form = UserCreationForm()
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        # form = UserCreationForm(request.POST)
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' +user)
+
+            return redirect('login')
+
+    context = {'form': form}
+    return render(request, 'accounts/register.html', context)
+
+def loginPage(request):
+    context = {}
+    return render(request, 'accounts/login.html', context)
 
 def home(request) :
     orders = Order.objects.all()
@@ -36,26 +62,37 @@ def customer(request, pk_test):
     orders = customer.order_set.all()
     order_count = orders.count()
 
+    myFilter = OrderFilter(request.GET, queryset=orders)
+    orders = myFilter.qs
+
     context = {'customer': customer,
                 'orders':orders,
-                'order_count': order_count
+                'order_count': order_count,
+                'myFilter': myFilter
                 }
     return render(request, 'accounts/customer.html', context)
 
 
-def createOrder(request):
+def createOrder(request, pk):
+    OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status'), extra=5)
 
-    form = OrderForm()
+    customer = Customer.objects.get(id=pk)
+
+    formset = OrderFormSet(queryset=Order.objects.none(), instance=customer)
+    # form = OrderForm(initial={'customer':customer})
+    # initial will give the customer name by defaulth in the customer field of place order.
 
     if request.method == 'POST':
         # print('Printing: ', request.POST)
         # request.POST (output) --> <QueryDict: {'csrfmiddlewaretoken': ['tAdtLaC381HmiUieRoZifiVrxhInabBjYQRFZZhGGkJCmBlFIrn9g6b1jweaMNwx'], 'customer': ['1'], 'product': ['2'], 'status': ['Delivered'], 'submit': ['Submit']}>
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            form.save()
+        # form = OrderForm(request.POST)
+        formset = OrderFormSet(request.POST, instance=customer)
+
+        if formset.is_valid():
+            formset.save()
             return redirect('/')
 
-    context = {'form': form}
+    context = {'formset': formset}
     return render(request, 'accounts/order_form.html', context)
 
 def updateOrder(request, pk):
